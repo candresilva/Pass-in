b81396a72b93b30fb946bb5bdab07f8806320d74
Carlos André de Oliveira <carlos.oliveira@stc.ma.gov.br>
2024-04-02 23:19:31 -0300

    feat: Adding try and exceptions in events repo

--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -4,19 +4,20 @@ from .events_repository import EventsRepository
 
 db_connection_handler.__connect_to_db__()
 
-@pytest.mark.skip(reason = "Já inserido. Não quero testar novamente")
+#@pytest.mark.skip(reason = "Já inserido. Não quero testar novamente")
 def test_insert_event():
-        event = {
-            "uuid" : "2o participante",
-            "title" : "rocketseat v2",
-            "details" : "passin-application backend",
-            "slug" : "decolando no foguete novamente",
-            "maximum_attendees" : 250
-            }
-        event_repository = EventsRepository()
-        response = event_repository.insert_event(event)
-        print (response)
+            event = {
+                "uuid" : "2o participante",
+                "title" : "rocketseat v2",
+                "details" : "passin-application backend",
+                "slug" : "decolando no foguete novamente",
+                "maximum_attendees" : 250
+                }
+            event_repository = EventsRepository()
+            response = event_repository.insert_event(event)
+            print (response)
 
+#@pytest.mark.skip(reason = "Não quero testar agora")
 def test_get_event_byID():
         event_id = "2o participante"
         event_repository = EventsRepository()
